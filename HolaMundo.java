934a24711b62672b75de15604817c12f54d12ddf
SaraLinI12 <princesnicole051@hotmail.com>
2022-04-28 14:14:22 -0500

    mi primer trabajo

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+public class HolaMundo{
+
+public static void main (String[] elias){
+//Variables y Datos de Entrada
+int lA=4;
+int lB=5;
+int lC=3;
+// Proceso
+int v=lA*lB*lC;
+//Datos Salida
+System.out.println("El volumen es: "+v);
+
+}
+
+}
